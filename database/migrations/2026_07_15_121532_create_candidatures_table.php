bc5b6f3b78c6011aa3c0587af7ce9e5585a4d338
oussamamalih <osmmlh816@gmail.com>
2026-07-15 14:12:05 +0100

    pushing the project

---
+++ database/migrations/2026_07_15_121532_create_candidatures_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+           Schema::create('candidatures', function (Blueprint $table) {
+        $table->id();
+
+        $table->foreignId('user_id')
+              ->constrained()
+              ->cascadeOnDelete();
+
+        $table->foreignId('offre_id')
+              ->constrained()
+              ->cascadeOnDelete();
+
+        $table->enum('status', [
+            'pending',
+            'accepted',
+            'refused'
+        ])->default('pending');
+
+        $table->timestamps();
+
+        $table->unique(['user_id', 'offre_id']);
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('candidatures');
+    }
+};
